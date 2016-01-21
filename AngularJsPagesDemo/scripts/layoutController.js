7f4e908d5bba479bb29d95fd738ec8591bf18bd6
pdschuller <phil@pdschuller.com>
2016-01-21 10:44:37 -0500

    Init commit AngularJsPages Demo
    There is a controllers module with a controller object in it.

---
+++ AngularJsPagesDemo/scripts/layoutController.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Phil on 1/21/2016.
+ */
+
+var pdsSampleApp = angular.module("pdsSampleApp", [
+    // 'ngRoute',
+    'controllersModule'
+]);
+
+var controllersModule = angular.module('controllersModule', []);
+
+controllersModule.controller('myController', ['$scope', function($scope){
+    $scope.pdsModel = {
+        myTitle: "snot4"
+    }}]);
+
+
+
+
+
+//pdsSampleApp.controller("myController", function($scope){
+//    $scope.pdsModel = {
+//        myTitle: "snot3"
+//    };
+//
+//});
