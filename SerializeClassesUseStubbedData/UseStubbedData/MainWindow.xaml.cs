66780f423ecd12d259247928feb42cd76c3456ef
pdschuller <phil@pdschuller.com>
2018-06-26 14:06:52 -0400

    minor edits

--- SerializeClassesUseStubbedData/UseStubbedData/MainWindow.xaml.cs
+++ SerializeClassesUseStubbedData/UseStubbedData/MainWindow.xaml.cs
@@ -6,28 +6,13 @@ using System.Data;
 using System.Data.SqlClient;
 using System.IO;
 using System.IO.Compression;
-using System.Linq;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using UseStubbedData.Data;
 using UseStubbedData.StubData;
 
 namespace UseStubbedData
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
