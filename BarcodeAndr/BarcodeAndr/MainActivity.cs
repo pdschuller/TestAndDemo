947e6abb89310ef427449c04b579332f5feb10df
pdschuller <phil@pdschuller.com>
2018-07-21 22:28:19 -0400

    initial commit

---
+++ BarcodeAndr/BarcodeAndr/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace BarcodeAndr
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, BottomNavigationView.IOnNavigationItemSelectedListener
+    {
+        private TextView MyTextView;
+
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.navigation_home:
+                    MyTextView.SetText(Resource.String.title_home);
+                    return true;
+                case Resource.Id.navigation_dashboard:
+                    MyTextView.SetText(Resource.String.title_dashboard);
+                    return true;
+                case Resource.Id.navigation_notifications:
+                    MyTextView.SetText(Resource.String.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // next we grab the xaml ... er, I mean the axml ... aka layout
+            SetContentView(Resource.Layout.activity_main);
+            // wire up 
+            MyTextView = FindViewById<TextView>(Resource.Id.MyTextView);
+            BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
+            navigation.SetOnNavigationItemSelectedListener(this);
+        }
+    }
+}
+
