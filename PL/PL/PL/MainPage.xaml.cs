9e17ffa6098ace40cae22a2eade198821a6783f6
pdschuller <phil@pdschuller.com>
2018-09-06 16:36:26 -0400

    stable but not GetData; throws Snix error

--- PL/PL/PL/MainPage.xaml.cs
+++ PL/PL/PL/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PL.PalletLoadedDataSetTableAdapters;
+using System;
 using System.Collections.Generic;
 // using System.Data;
 using System.Data.SqlClient;
@@ -14,9 +15,29 @@ namespace PL
         public MainPage()
         {
             InitializeComponent();
-            // works  DataTable dt = new DataTable();
-            // this is wrongly trying to use a namespace like a type
-            // System.Data.SqlClient pp = new System.Data.SqlClient();
+            
+        }
+
+        private void DisplayItems(object sender, EventArgs e)
+        {
+            var rr = e;
+            ClientBarcodeTypeTableAdapter adpt = new ClientBarcodeTypeTableAdapter();
+            // adpt.Connection.ConnectionString = @"Data Source=PHIL_PC\SQLEXPRESS2017;Initial Catalog=wjbeitler;Integrated Security=True";
+            adpt.Connection = new SqlConnection(@"Data Source=PHIL_PC\SQLEXPRESS2017;Initial Catalog=wjbeitler;Integrated Security=True");
+            // adpt.Connection.ConnectionString = @"Data Source=PHIL_PC\SQLEXPRESS2017;user id=sa;password=qweqwe101622;Initial Catalog=wjbeitler";
+            //                                   Data Source={SQL_INSTANCE_NAME};user id={SQL_USERNAME};password={SQL_PASS};Initial Catalog={DB:NAME};
+            //                                   data source={server_address};initial catalog={database_name};user id={user_id};password={password};Connect Timeout={seconds}
+            // keyword not supported   adpt.Connection.ConnectionString = @"PHIL_PC\SQLEXPRESS2017;Initial Catalog=wjbeitler;Integrated Security=True";
+
+            try
+            {
+                // BcTypesListView.ItemsSource = PalletLoadedDataSet.ClientBarcodeTypeDataTable;  // adpt.GetData();
+                BcTypesListView.ItemsSource = adpt.GetData();
+            }
+            catch (Exception exp)
+            {
+                var r = exp;
+            }
         }
     }
 }
