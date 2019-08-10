d7287421b56e9bb9b39e3ca076011d1a25d16014
pdschuller <phil@pdschuller.com>
2019-08-10 12:00:30 -0400

    the Lite Db thing is working fine

--- SQLiteDemo/SQLiteDemo/MainActivity.cs
+++ SQLiteDemo/SQLiteDemo/MainActivity.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Android.App;
 using Android.OS;
 using Android.Runtime;
@@ -6,6 +7,9 @@ using Android.Support.Design.Widget;
 using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
+using SQLite;
+using SQLitePCL;
+// using Mono. .Android.Data.Sqlite;
 
 namespace SQLiteDemo
 {
@@ -23,6 +27,40 @@ namespace SQLiteDemo
 
             FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
             fab.Click += FabOnClick;
+            Button MyButton = FindViewById<Button>(Resource.Id.button1);
+            MyButton.Click += MyButton_Click;
+        }
+
+        private void MyButton_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Creating database, if it doesn't already exist");
+            string dbPath = Path.Combine(
+                    System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal),
+                    "ormdemo.db3");
+            var db = new SQLiteConnection(dbPath);
+            db.CreateTable<Stock>();
+            if (db.Table<Stock>().Count() == 0)
+            {
+                // only insert the data if it doesn't already exist
+                var newStock = new Stock();
+                newStock.Symbol = "AAPL";
+                db.Insert(newStock);
+                newStock = new Stock();
+                newStock.Symbol = "GOOG";
+                db.Insert(newStock);
+                newStock = new Stock();
+                newStock.Symbol = "MSFT";
+                db.Insert(newStock);
+            }
+            Console.WriteLine("Reading data");
+            var table = db.Table<Stock>();
+            foreach (var s in table)
+            {
+                Console.WriteLine(s.Id + " " + s.Symbol);
+            }
+            
+
+
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
@@ -49,5 +87,16 @@ namespace SQLiteDemo
                 .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
         }
 	}
+
+
+    [Table("Items")]
+    public class Stock
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; set; }
+        [MaxLength(8)]
+        public string Symbol { get; set; }
+    }
+
 }
 
